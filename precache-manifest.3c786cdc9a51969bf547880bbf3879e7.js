d856529afa2845316dd924c9926fd03ed9febe74
Ishwor Khadka <Khadkaishwor44@gmail.com>
2020-02-15 23:49:44 +1100

    Updates

---
+++ precache-manifest.3c786cdc9a51969bf547880bbf3879e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cb9dac918b5164f3a755a086f0f716",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8709035f9dd7b6d7c335",
+    "url": "/monsters-rolodex/static/css/main.4303c5d4.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8709035f9dd7b6d7c335",
+    "url": "/monsters-rolodex/static/js/main.9f9b83e1.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
